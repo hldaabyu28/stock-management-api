f232bad67b98bedf55d7bb3bf2ee56afcdf649eb
Hilda Abiyyu Rahmadani <danirahmadani91@gmail.com>
2025-09-19 10:24:53 +0700

    rev deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { Barang } from './barang/barang.entitiy';
 import { Kategori } from './kategori/kategori.entitiy';
 import { BarangModule } from './barang/barang.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root', 
-      password: '',     
-      database: 'product',
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
       entities: [Barang, Kategori],
-      synchronize: true, 
+      synchronize: true, // ⚠️ Hanya aktifkan di dev
+      ssl: {
+        rejectUnauthorized: false, // penting untuk koneksi Supabase
+      },
     }),
     BarangModule,
   ],
