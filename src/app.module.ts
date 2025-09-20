86d9caa66e258d1c27f70b2dbafaffb5f3b54a4f
Hilda Abiyyu Rahmadani <danirahmadani91@gmail.com>
2025-09-20 20:35:06 +0700

    rev env vv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,29 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BarangModule } from './barang/barang.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Barang } from './barang/barang.entitiy';
 import { Kategori } from './kategori/kategori.entitiy';
+import { BarangModule } from './barang/barang.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
+      isGlobal: true, 
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      entities: [Barang, Kategori],
-      synchronize: false, 
-      ssl: {
-        rejectUnauthorized: false,
-      },
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST'),
+        port: parseInt(config.get<string>('DB_PORT', '4000')),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASS'),
+        database: config.get<string>('DB_NAME'),
+        ssl: { rejectUnauthorized: true },
+        entities: [Barang, Kategori],
+        synchronize: false,
+      }),
     }),
     BarangModule,
   ],
